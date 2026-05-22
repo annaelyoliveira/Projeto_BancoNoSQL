e8b037e8fa50b6e1535d995c5b8aaee94ad6b1b3
Andrey Rian <andreyrian581@gmail.com>
2026-05-21 23:50:19 -0300

    estrutura inicial

--- src/main/java/atendimento_api/dto/ConsultaDTO.java
+++ src/main/java/atendimento_api/dto/ConsultaDTO.java
@@ -5,15 +5,12 @@ import lombok.Data;
 @Data
 public class ConsultaDTO {
 
-    private String cpf;
-
-    private String dataConsulta;
-
     private String nome;
-
+    private String cpf;
     private String telefone;
-
+    private String dataConsulta;
+    private String especialidade;
     private String medico;
 
-    private String especialidade;
+    public ConsultaDTO() {};
 }
--- src/main/java/atendimento_api/service/ConsultaService.java
+++ src/main/java/atendimento_api/service/ConsultaService.java
@@ -6,30 +6,70 @@ import atendimento_api.repository.ConsultaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ConsultaService {
 
-    private final ConsultaRepository repository;
+    private final ConsultaRepository consultaRepository;
 
-    public void salvar(ConsultaDTO dto) {
+    public void salvar(ConsultaDTO consultaDTO) {
 
         Consulta consulta = new Consulta();
 
-        consulta.setPk("PACIENTE#" + dto.getCpf());
+        consulta.setPk("PACIENTE#" + consultaDTO.getCpf());
 
-        consulta.setSk("CONSULTA#" + dto.getDataConsulta());
+        consulta.setSk("CONSULTA#" + consultaDTO.getDataConsulta());
 
-        consulta.setNome(dto.getNome());
+        consulta.setNome(consultaDTO.getNome());
 
-        consulta.setTelefone(dto.getTelefone());
+        consulta.setTelefone(consultaDTO.getTelefone());
 
-        consulta.setMedico(dto.getMedico());
+        consulta.setMedico(consultaDTO.getMedico());
 
-        consulta.setEspecialidade(dto.getEspecialidade());
+        consulta.setEspecialidade(consultaDTO.getEspecialidade());
 
         consulta.setStatus("AGUARDANDO");
 
-        repository.salvar(consulta);
+        consultaRepository.salvar(consulta);
+    }
+
+    public List<ConsultaDTO> buscar(String cpf, String dataConsulta) {
+        List<Consulta> consultas = consultaRepository.buscar(cpf, dataConsulta);
+
+        return  consultas.stream()
+                .map(this::converterDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void atualizar(String cpf, String dataConsulta, String especialidade, String status) {
+        consultaRepository.atualizar(cpf, dataConsulta, especialidade, status);
+    }
+
+
+    public void deletar(String cpf, String dataConsulta) {
+        consultaRepository.deletar(cpf, dataConsulta);
+    }
+
+    private ConsultaDTO converterDTO(Consulta consulta) {
+        ConsultaDTO consultaDTO = new ConsultaDTO();
+
+        consultaDTO.setNome(consulta.getNome());
+        consultaDTO.setTelefone(consulta.getTelefone());
+
+        String cpf = consulta.getPk().replace("PACIENTE#", "");
+        consultaDTO.setCpf(cpf);
+
+        String data = consulta.getSk().replace("CONSULTA#", "");
+        consultaDTO.setDataConsulta(data);
+
+        consultaDTO.setEspecialidade(consulta.getEspecialidade());
+        consultaDTO.setMedico(consulta.getMedico());
+
+        return  consultaDTO;
     }
 }
